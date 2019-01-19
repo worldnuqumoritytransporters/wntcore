3a175e19894e50c9b4bea2e0e9d45e541ac7335d
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2019-01-20 05:01:08 +0800

    Genesis updated

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@
 
 exports.COUNT_WITNESSES = 12;
 exports.MAX_WITNESS_LIST_MUTATIONS = 1;
-exports.TOTAL_WHITEBYTES = 1e15;
+exports.TOTAL_WHITEBYTES = 1e16;
 exports.MAJORITY_OF_WITNESSES = (exports.COUNT_WITNESSES%2===0) ? (exports.COUNT_WITNESSES/2+1) : Math.ceil(exports.COUNT_WITNESSES/2);
 exports.COUNT_MC_BALLS_FOR_PAID_WITNESSING = 100;
 
@@ -12,8 +12,9 @@ exports.alt = '1';
 
 exports.bTestnet = (exports.alt === '2' && exports.version === '1.0t');
 
-exports.GENESIS_UNIT = exports.bTestnet ? 'TvqutGPz3T4Cs6oiChxFlclY92M2MvCvfXR5/FETato=' : 'oj8yEksX9Ubq7lLc+p6F2uyHUuynugeVq4+ikT67X6E=';
-exports.BLACKBYTES_ASSET = exports.bTestnet ? 'LUQu5ik4WLfCrr8OwXezqBa+i3IlZLqxj2itQZQm8WY=' : 'qO2JsiuDMh/j+pqJYZw3u82O71WjCDf0vTNvsnntr8o=';
+exports.GENESIS_UNIT = exports.bTestnet ? 'QHm8/S+YRNKbC1cUoOiIEkOrnCmryqUMBTP+nWfJXsU=' : 'W3HhzvhShvSbnaP7qcOQr+j2bq3naeJXRdp5PXrbjKw=';
+exports.BLACKBYTES_ASSET = exports.bTestnet ? 'LUQu5ik4WLfCrr8OwXezqBa+i3IlZLqxj2itQZQm8WY=' : 'WHzTwIpqkxWJIPkiiHI9+JP4DjTnWuavvbyZ6nbMuYs=';
+
 
 exports.HASH_LENGTH = 44;
 exports.PUBKEY_LENGTH = 44;
